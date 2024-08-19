bc838003f2ca4bc70a1e1be402f24989099b77c5
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-08-19 15:31:13 -0300

    fazendo o backend pegar

--- Tech/backend/backend.js
+++ Tech/backend/backend.js
@@ -1,16 +1,17 @@
 const jsonServer = require('json-server');
 const cors = require('cors');
+const path = require('path');
 
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(cors());
+server.use(router);
 
 const PORT = process.env.PORT || 8000;
 
-server.use(cors());
-server.use(middlewares);
-server.use(router);
-
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
+  console.log(`JSON Server is running on port ${PORT}`);
 });
