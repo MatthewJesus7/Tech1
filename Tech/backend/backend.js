802aa0b82419135b00dbeb5fb6efa1bf3066f65a
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-08-19 20:05:12 -0300

    Fazendo pegar

--- Tech/backend/backend.js
+++ Tech/backend/backend.js
@@ -1,22 +1,20 @@
-import jsonServer from 'json-server';
-import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
+const jsonServer = require('json-server');
+const cors = require('cors');
+const path = require('path');
 
-// Para obter o __dirname no ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// Criação do servidor
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(cors());
-server.use(router);
-
 const PORT = process.env.PORT || 8000;
 
+// Middleware para CORS
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+
+// Início do servidor
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
