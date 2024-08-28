29fa05c2bd788efb44ad013e2a29d0166c442a04
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-08-28 18:23:32 -0300

    Filtro funcionando (SO)

--- Tech/src/components/items/Buttons/FIlterButton.jsx
+++ Tech/src/components/items/Buttons/FIlterButton.jsx
@@ -1,7 +1,7 @@
 function FilterButton({ handleOnClick }) {
     return(
         <button onClick={handleOnClick}
-        className="size-14 bg-white border rounded-md p-5 shadow-md; hover:shadow-lg ease-out duration-300 overflow-hidden ">
+        className="w-20 h-10 bg-white border rounded-md shadow-md; hover:shadow-lg ease-out duration-300 overflow-hidden mr-1 sm:mr-5">
 
         </button>
     )
