a967d0eb8479a4212952dc8b245081b9e759b598
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-09-15 13:01:09 -0300

    atualização do banco de dados

--- Tech/src/firebaseAdmin.js
+++ Tech/src/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("cards");
+const serviceAccount = require("C:/Users/Michele/Downloads/commercetechback-firebase-adminsdk-by35b-3da5bf3da1.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
