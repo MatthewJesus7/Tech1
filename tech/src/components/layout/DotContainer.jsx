4891d89658a4ccf5656b7b0357ee1f89bb5f81ca
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-09-17 18:06:08 -0300

    carrossel + dots finalizado

--- tech/src/components/layout/DotContainer.jsx
+++ tech/src/components/layout/DotContainer.jsx
@@ -2,7 +2,7 @@ import DotBar from "../items/DotBar";
 
 const DotContainer = ({dots, currentIndex}) => {
     return(
-        <div className='flex absolute bottom-32
+        <div className='flex absolute bottom-36
         left-1/2 -translate-x-1/2'>
              {dots.map((dot, index) => (
             <DotBar key={index} isActive={index === currentIndex} />
