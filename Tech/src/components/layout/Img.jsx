5441849c9d5a8f63859fff3c43e01274f93dc69f
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-09-17 22:25:54 -0300

    finalizando hero carossel + ajustes de design

--- Tech/src/components/layout/Img.jsx
+++ Tech/src/components/layout/Img.jsx
@@ -1,10 +1,10 @@
-function Img({ children, customclass, backgroundImage, custom }) {
+function Img({ children, customclass, backgroundImage, backgroundSize, custom }) {
     return(
             <div className={` ${custom}`}>
                 <div
                 className={` min-w-full h-96 lg:size-[560px] bg-opacity-75 flex justify-center items-top ${customclass}`}
                 style={{
-                    backgroundImage: backgroundImage, backgroundSize: 'cover', backgroundPosition: 'center',
+                    backgroundImage: backgroundImage, backgroundSize: backgroundSize, backgroundPosition: 'center',
                     backgroundRepeat: 'no-repeat'
                 }}
                 >
