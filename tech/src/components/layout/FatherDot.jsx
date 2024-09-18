df3780ec96e8eebc938fe933259363c2bc72cd5c
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-09-18 09:54:13 -0300

    testando

--- tech/src/components/layout/FatherDot.jsx
+++ tech/src/components/layout/FatherDot.jsx
@@ -5,7 +5,8 @@ const FatherDot = ({dots, currentIndex}) => {
         <div className='flex absolute bottom-36
         left-1/2 -translate-x-1/2'>
              {dots.map((dot, index) => (
-            <DotBar key={index} isActive={index === currentIndex} />
+            <DotBar
+            key={index} isActive={index === currentIndex} />
         ))}
         </div>
     )
