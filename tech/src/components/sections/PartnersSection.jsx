c49aa46c389220b246a7f2d7ed015330ce1d9b48
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-09-21 22:29:56 -0300

    arrumando partners

--- tech/src/components/sections/PartnersSection.jsx
+++ tech/src/components/sections/PartnersSection.jsx
@@ -9,11 +9,11 @@ function PartnersSection() {
     const partnersCards = [
         {
             type: "card medium_card",
-            backgroundImage: "url('https://upload.wikimedia.org/wikipedia/commons/4/41/Amazon_PNG6.png')"
+            image_url: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Amazon_PNG6.png'
         },
         {
             type: "card medium_card min-[1px]:bg-[#FFE700]",
-            backgroundImage: "url('https://www.otabuleiro.com.br/storage/images/cache/62c277460851cdb2.png')"
+            image_url: 'https://www.otabuleiro.com.br/storage/images/cache/62c277460851cdb2.png'
         },
     ]
 
